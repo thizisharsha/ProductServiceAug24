22c6f0969b50ca52791492156d345ad5b69c4fd2
Harsh <harsh@example.com>
2024-08-30 15:41:03 +0530

    Added Spring data jpa to project

--- ProductServiceAug24/src/main/java/com/example/ProductServiceAug24/models/Product.java
+++ ProductServiceAug24/src/main/java/com/example/ProductServiceAug24/models/Product.java
@@ -1,9 +1,13 @@
 package com.example.ProductServiceAug24.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
-
+@Entity
 public class Product {
+    @Id
+    private long id;
     String name;
     String category;
     String description;
@@ -30,5 +34,13 @@ public class Product {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 }
